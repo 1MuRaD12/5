6fdae661479a5ec0a8e6af11c16598b7aeea45d5
1MuRaD12 <tu71tfsws@code.edu.az>
2022-05-01 06:10:03 +0300

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Book
+    {
+        public string Name { get; set; }
+
+        public static int Id;
+
+        public string AuthorName { get; set; }
+
+        public int PageCount { get; set; }
+
+        public string Code { get; set; }
+
+        public Book( string name, string authorname ,int pagecount , string code)
+        {
+            Id++;
+            Name = name;
+            AuthorName = authorname;
+            PageCount = pagecount;
+
+            Code = code.Remove(1).ToUpper() + Id;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Library.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Library
+    {
+        public List<Book> books = new List<Book>();
+
+
+
+        public List<Book> FindAllBooksByName(string value)
+        {
+            return books.FindAll(book => book.Name.Contains(value));
+        }
+        public void RemoveAllBooksByNme(string value)
+        {
+            books.RemoveAll(book => book.Name.Contains(value));
+        }
+        public List<Book> SearchBooks(string value)
+        {
+            return books.FindAll(book => book.Name.Contains(value) || book.AuthorName.Contains(value) || book.PageCount.ToString().Contains(value));
+        }
+        public List<Book> FindAllBooksByPageCountRange(int min, int max)
+        {
+            return books.FindAll(book => book.PageCount >= min && book.PageCount <= max);
+        }
+        public void RemoveBookByCode(string value)
+        {
+            Book want = books.Find(book => book.Code == value);
+            if (want != null)
+            {
+                books.Remove(want);
+            }
+        }
+    }
+}
